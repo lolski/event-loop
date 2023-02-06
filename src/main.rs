36957e81f907a38c5e53fd2e6184263edd9ec0ce
Ganeshwara Hananda <ganesh@vaticle.com>
2023-02-06 18:52:56 +0000

    More sophisticated event types

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,61 @@ use std::thread::JoinHandle;
 
 fn main() {
     let el = EventLoop::create();
-    el.submit(Box::new(|| { println!("hello"); 5 }));
+    el.submit_task(Box::new(|| { println!("hello"); 5 }));
+    el.submit_task(Box::new(|| { println!("world"); 5 }));
+    el.submit_task(Box::new(|| { println!("!"); 5 }));
     el.stop();
 }
 
 
+enum Event {
+    Task(Box<dyn FnOnce() -> i32 + Send>),
+    Stop()
+}
+
 struct EventLoop {
-    send_chan: Sender<Box<dyn FnOnce() -> i32 + Send>>,
+    send_chan: Sender<Event>,
     loop_thread: JoinHandle<()>
 }
 
 impl EventLoop {
     pub fn create() -> Self {
         let (send_chan, recv_chan) = channel();
-        let loop_thread = Self::spawn_loop_thread(recv_chan);
+        let loop_thread = Self::spawn_event_loop_thread(recv_chan);
         EventLoop { send_chan, loop_thread }
     }
 
-    fn spawn_loop_thread(recv_chan: Receiver<Box<dyn FnOnce() -> i32 + Send>>) -> JoinHandle<()> {
+    fn spawn_event_loop_thread(recv_chan: Receiver<Event>) -> JoinHandle<()> {
         thread::spawn(move || {
             loop {
-                let task = recv_chan.recv();
-                if task.is_ok() {
-                    let t: Box<dyn FnOnce() -> i32> = task.unwrap();
-                    let status = t();
-                    if status == -666 {
-                        break;
-                    }
+                let event = recv_chan.recv();
+                if event.is_ok() {
+                    match event.unwrap() {
+                        Event::Task(t) => {
+                            let _ = t();
+                        }
+                        Event::Stop() => {
+                            break;
+                        }
+                    };
                 } else {
-                    let e: std::sync::mpsc::RecvError = task.err().unwrap();
+                    let e: std::sync::mpsc::RecvError = event.err().unwrap();
                     println!("An error occurred: {}", e)
                 }
             }
         })
     }
 
-    pub fn submit(self: &Self, task: Box<dyn FnOnce() -> i32 + Send>) -> () {
-        let _ = self.send_chan.send(task);
+    pub fn submit_task(self: &Self, task: Box<dyn FnOnce() -> i32 + Send>) -> () {
+        let _ = self.submit(Event::Task(task));
+    }
+
+    fn submit(self: &Self, event: Event) -> () {
+        let _ = self.send_chan.send(event);
     }
 
     pub fn stop(self: Self) -> () {
-        self.submit(Box::new(|| -666));
+        let _ = self.submit(Event::Stop());
         let _ = self.loop_thread.join();
     }
 }
