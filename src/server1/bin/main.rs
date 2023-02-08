60f2e0c2a7fe84573dc1424fadeed012f67d8cb7
Ganeshwara Hananda <ganesh@vaticle.com>
2023-02-08 19:16:25 +0000

    wip - publisher

--- src/server1/bin/main.rs
+++ src/server1/bin/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("server1: hello world!");
+use google_cloud_pubsub::client::{Client, ClientConfig};
+
+#[tokio::main]
+async fn main() {
+    let mut client = Client::default().await.unwrap();
+    let topic = client.topic("test");
+    if !topic.exists(None, None) {
+        topic.create(None, None, None);
+    }
+    topic.subscriptions(None, None, None).await;
+
 }
