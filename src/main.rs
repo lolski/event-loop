8c664252af5280aa54b893f23373b874779343fe
Ganeshwara Hananda <ganesh@vaticle.com>
2023-02-14 12:24:34 +0000

    Make submit() return Receiver

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
-use futures::Future;
-
-use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::mpsc::{channel, Receiver, Sender, sync_channel, SyncSender};
 use std::thread;
 use std::thread::JoinHandle;
 
 fn main() {
     let el = EventLoop::create();
-    el.submit_task(Box::new(|| { println!("hello"); 5 }));
-    el.submit_task(Box::new(|| { println!("world"); 5 }));
-    el.submit_task(Box::new(|| { println!("!"); 5 }));
+    let res = el.submit_task(Box::new(|| { println!("executing task 1"); 5 }));
+    let res2 = el.submit_task(Box::new(|| { println!("executing task 2"); 6 }));
+    let res3 = el.submit_task(Box::new(|| { println!("executing task 3"); 7 }));
+    println!("result: {}", res.recv().unwrap());
+    println!("result: {}", res2.recv().unwrap());
+    println!("result: {}", res3.recv().unwrap());
     el.stop();
 }
 
-
 enum Event {
-    Task(
-        Box<dyn FnOnce() -> i32 + Send>,
-        Option<Box<dyn Future<Output=i32> + Send>>
-    ),
+    Task(Box<dyn FnOnce() -> i32 + Send>, Option<SyncSender<i32>>),
     Stop()
 }
 
@@ -39,10 +36,10 @@ impl EventLoop {
                 let event = recv_chan.recv();
                 if event.is_ok() {
                     match event.unwrap() {
-                        Event::Task(task, future_opt) => {
+                        Event::Task(task, s_opt) => {
                             let result = task();
-                            if let Some(fut) = future_opt {
-
+                            if let Some(s) = s_opt {
+                                s.send(result).unwrap();
                             }
                         }
                         Event::Stop() => {
@@ -57,11 +54,10 @@ impl EventLoop {
         })
     }
 
-    pub fn submit_task(self: &Self, task: Box<dyn FnOnce() -> i32 + Send>) -> Box<dyn Future<Output=i32> + Send> {
-        let future: Box<dyn Future<Output=i32> + Send> = Box::new(async { 5 } );
-        let _ = self.submit(Event::Task(task, Some(future)));
-        let future: Box<dyn Future<Output=i32> + Send> = Box::new(async { 5 } );
-        future
+    pub fn submit_task(self: &Self, task: Box<dyn FnOnce() -> i32 + Send>) -> Receiver<i32> {
+        let (send_chan, recv_chan) = sync_channel::<i32>(1);
+        let _ = self.submit(Event::Task(task, Some(send_chan)));
+        recv_chan
     }
 
     fn submit(self: &Self, event: Event) -> () {
