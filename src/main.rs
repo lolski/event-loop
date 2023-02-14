09b6f3aa3f1693f05effbb774957dda56d9456d0
Ganeshwara Hananda <ganesh@vaticle.com>
2023-02-14 16:36:20 +0000

    Rename EventLoop to TaskExecutor

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::thread::{JoinHandle, sleep};
 use std::time::Duration;
 
 fn main() {
-    let el = EventLoop::create();
+    let el = TaskExecutor::create();
     let res = el.submit_task(Box::new(|| { println!("executing task 1"); 5 }));
     let res2 = el.submit_task(Box::new(|| { println!("executing task 2"); 6 }));
     let res3 = el.submit_task(Box::new(|| { println!("executing task 3"); 7 }));
@@ -15,41 +15,45 @@ fn main() {
     el.stop();
 }
 
-enum Event {
-    Task { task: Box<dyn FnOnce() -> i32 + Send>, sender_opt: Option<Sender<i32>> },
-    Stop()
+enum Message {
+    Task { task: Box<dyn FnOnce() -> i32 + Send>, send_chan_opt: Option<Sender<i32>> },
+    Stop { send_chan: Sender<()> }
 }
 
-struct EventLoop {
-    send_chan: Sender<Event>,
-    loop_thread: JoinHandle<()>
+struct TaskExecutor {
+    send_chan: Sender<Message>,
+    execution_thread: JoinHandle<()>
 }
 
-impl EventLoop {
+impl TaskExecutor {
     pub fn create() -> Self {
         let (send_chan, recv_chan) = channel();
-        let loop_thread = Self::spawn_event_loop_thread(recv_chan);
-        EventLoop { send_chan, loop_thread }
+        let execution_thread = Self::spawn_execution_thread(recv_chan);
+        TaskExecutor { send_chan, execution_thread }
     }
 
-    fn spawn_event_loop_thread(recv_chan: Receiver<Event>) -> JoinHandle<()> {
+    fn spawn_execution_thread(recv_chan: Receiver<Message>) -> JoinHandle<()> {
         thread::spawn(move || {
             loop {
                 match recv_chan.recv() {
-                    Ok(Event::Task { task, sender_opt }) => {
+                    Ok(Message::Task { task, send_chan_opt }) => {
                         let result = task();
-                        if let Some(sender) = sender_opt {
-                            let send = sender.send(result);
+                        if let Some(send_chan) = send_chan_opt {
+                            let send = send_chan.send(result);
                             if let Err(e) = send {
                                 println!("An error occurred when sending the result of a task the caller: {}", e);
                             }
                         }
                     },
-                    Ok(Event::Stop()) => {
-                        break;
-                    },
                     Err(e) => {
-                        println!("An error occurred: {}", e)
+                        println!("An error occurred while receiving an event: {}", e)
+                    },
+                    Ok(Message::Stop { send_chan: sender }) => {
+                        let send = sender.send(());
+                        if let Err(e) = send {
+                            println!("An error occurred when stopping the event loop: {}", e);
+                        }
+                        break;
                     }
                 }
             }
@@ -58,16 +62,18 @@ impl EventLoop {
 
     pub fn submit_task(self: &Self, task: Box<dyn FnOnce() -> i32 + Send>) -> Receiver<i32> {
         let (send_chan, recv_chan) = channel();
-        let _ = self.submit(Event::Task { task, sender_opt: Some(send_chan) });
+        let _ = self.submit(Message::Task { task, send_chan_opt: Some(send_chan) });
         recv_chan
     }
 
-    fn submit(self: &Self, event: Event) -> () {
-        let _ = self.send_chan.send(event);
+    pub fn stop(self: Self) -> () {
+        let (send_chan, recv_chan) = channel();
+        self.submit(Message::Stop { send_chan } );
+        recv_chan.recv().unwrap();
+        let _ = self.execution_thread.join();
     }
 
-    pub fn stop(self: Self) -> () {
-        let _ = self.submit(Event::Stop());
-        let _ = self.loop_thread.join();
+    fn submit(self: &Self, event: Message) -> () {
+        let _ = self.send_chan.send(event);
     }
 }
